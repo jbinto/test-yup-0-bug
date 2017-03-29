6ce1ff188dbec30a475f7b46e7bb67f10f08cd9f
Jesse Buchanan <hello@jessebuchanan.ca>
2017-03-29 11:09:32 -0400

    test demonstrating 0 interpolation bug

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import yup from 'yup'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+const validator = yup.object().shape({
+  price: yup.number().min(0)
+})
+
+it('returns the correct message', async () => {
+  const input = {
+    price: -3
+  }
+
+  let errorMessage
+  try {
+    await validator.validate(input, {
+      strict: true,
+      abortEarly: false,
+    })
+  } catch (validationError) {
+    errorMessage = validationError.errors[0]
+  }
+
+  expect(errorMessage).toEqual('price must be greater than or equal to 0')
+
+})
